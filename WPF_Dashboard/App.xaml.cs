158eec79362e809ec1dd66b9c5874db152d9b804
Knagwoo <limjp130@gmail.com>
2026-09-08 16:43:14 +0900

    Delete WPF_Dashboard directory

--- WPF_Dashboard/App.xaml.cs
+++
@@ -1,5 +0,0 @@
-using System.Windows;
-namespace SmartTrafficDashboard
-{
-    public partial class App : Application { }
-}
--- WPF_Dashboard/Models/EventLogItem.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartTrafficDashboard.Models
-{
-    public class EventLogItem
-    {
-        public string Time { get; set; }
-        public string Type { get; set; }
-        public string Message { get; set; }
-    }
-}
